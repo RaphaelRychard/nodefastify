b47450cd56263dc33bf0aa58c7073d3b85362e59
Raphael Rychard <raph.rych@gmail.br>
2024-06-22 17:32:20 -0400

    add sumarry

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,38 @@ import { prismaClient } from '../database/prismaClient'
 import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await prismaClient.transactionClinte.findMany()
+
+    return { transactions }
+  })
+
+  app.get('/:uuid', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      uuid: z.string().uuid(),
+    })
+
+    const { uuid } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await prismaClient.transactionClinte.findUnique({
+      where: { uuid },
+    })
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summaryResult = await prismaClient.transactionClinte.aggregate({
+      _sum: {
+        amount: true,
+      },
+    })
+
+    const summary = { amount: summaryResult._sum.amount }
+
+    return { summary }
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string().default('not title'),
@@ -15,12 +47,24 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      replay.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await prismaClient.transactionClinte.create({
       data: {
         uuid: randomUUID(),
         title,
         amount: type === 'credit' ? amount : amount * -1,
         type,
+        created_at: Date(),
+        sessoin_id: sessionId,
       },
     })
 
